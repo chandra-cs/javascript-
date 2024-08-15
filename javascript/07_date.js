764206fc4ae0ef154001d48814def9b1617741a8
chandra-cs <chandrasekharjena110@gmail.com>
2024-08-15 15:44:09 +0000

    container config done

---
+++ javascript/07_date.js
@@ -0,0 +1,40 @@
+// let myCreatedDate = new Date(2023,7,15);
+// console.log(`Today Date Is ${myCreatedDate.toDateString()}`);
+// console.log(`Today Date Is ${myCreatedDate.toTimeString()}`);
+
+//  let myNewDate = new Date(2024,11,26,16,27,48);
+// console.log(myNewDate.toLocaleString());
+
+// let anotherDate = new Date("2024-08-15");
+// console.log(`Today Date is ${anotherDate.toLocaleString()}`);
+
+//let myTimeStamp =  Date.now();
+// console.log(myTimeStamp);
+// console.log(myNewDate.getTime());
+
+
+// let ishaaAnsTime   = Date.now();
+// let chandraAnsTime = Date.now();
+
+// checkPosition(chandraAnsTime,ishaaAnsTime);
+
+// function checkPosition(a,b){
+
+//     if(chandraAnsTime.getTime>ishaaAnsTime.getTime)
+//         {
+//             console.log(`Chandra Is First`);
+            
+//         }
+//         else 
+//         {
+//             console.log(`Ishaa Is First`);
+//         }
+        
+// }
+
+ console.log(Math.floor(Date.now()/1000)) ;
+
+
+
+
+
